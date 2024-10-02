c3a4561a3fc1455c67e239ae2225d2a80fcfdaea
Juliendollar <164401175+Juliendollar@users.noreply.github.com>
2024-10-02 22:10:52 +0200

    Add files via upload

--- check_map2.c
+++ check_map2.c
@@ -6,7 +6,7 @@
 /*   By: jd-halle <jd-halle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 02:02:22 by jd-halle          #+#    #+#             */
-/*   Updated: 2024/10/01 04:53:18 by jd-halle         ###   ########.fr       */
+/*   Updated: 2024/10/02 22:02:00 by jd-halle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ void	check_1st_and_last_line(t_game *game)
 	int	j;
 
 	j = 0;
-	while (j < ft_strlen(game->map[0]) -1)
+	while (j < ft_strlen(game->map[0]) - 1)
 	{
 		if (game->map[0][j] != '1')
 			clean_exit(game, "1ere ligne ne contient pas que des 1\n");
